ead2e61a3a1cb050425af3ecfd4c793761d4e720
NamakulaAllen <143063952+NamakulaAllen@users.noreply.github.com>
2023-12-07 12:25:00 +0300

    Add files via upload
    question_one

---
+++ question_one.py
@@ -0,0 +1,21 @@
+def calculate_area(area, radius):
+    output = area*radius
+    print(f"The area of{area} and {radius} is = {output}")
+
+calculate_area(3.14, 5)
+
+ 
+def sum_of_numbers(area, radius, n):
+    output = area+radius+n
+    print(f"The sum of numbers {area} and {radius} and {n} is {output}")
+
+sum_of_numbers(3.14, 5,4)
+
+#(iii)
+numbers =[1,3,5,7,9]
+numbers[2] = 10
+new_number = tuple(numbers)
+print(new_number)
+
+#(iv)
+even_numbers = [2,4,6,8,10]
